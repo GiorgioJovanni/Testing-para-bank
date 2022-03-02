f3ccd29c8efb8ce678bd1adf61a26cbc52c8dbe0
masterlesa <37372843+masterlesa@users.noreply.github.com>
2022-03-02 13:08:56 +0200

    added restore_password_page

---
+++ Tests/REST API /restore_password_page.py
@@ -0,0 +1,10 @@
+import requests
+
+def test_check_status_code_equals_200():
+    response_restore_password_page = requests.get("https://parabank.parasoft.com/parabank/lookup.htm")
+    # assert response_restore_password_page.status_code == 200, 'Response Failed'
+    assert response_restore_password_page.status_code == 200
+    if response_restore_password_page:
+        print('Response OK')
+    else:
+        print('Response Failed')
