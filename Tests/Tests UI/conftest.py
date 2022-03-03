8b729f04769067b955a7870604f698d2bd395869
masterlesa <37372843+masterlesa@users.noreply.github.com>
2022-03-03 15:34:52 +0200

    Revert "Merge branch 'UI-tests' into Test"
    This reverts commit f127790901e94fa25d80f3e1de9616c51fb38c4e, reversing
    changes made to 0d55d27b83ff455b4d7d88b2a8ee903a541be822.

--- Tests/Tests UI/conftest.py
+++ Tests/Tests UI/conftest.py
@@ -1,11 +1,20 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture
-def browser():
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="ru",
+                 help="Choose language. For ex.: ru, es, en, fr")
+
+
+@pytest.fixture(scope="module")
+def browser(request):
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language}) # en-US
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
