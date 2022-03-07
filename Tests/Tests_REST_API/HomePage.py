6eb6dba21afc2d9d614a80d982ca6cdfe1788b02
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-03-07 17:03:39 +0300

    Created new tests

--- Tests/Tests_REST_API/HomePage.py
+++ Tests/Tests_REST_API/HomePage.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 
-class BaseParaBank:
+class ParaBankHomePage:
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
--- Tests/Tests_REST_API/test_base_page.py
+++ Tests/Tests_REST_API/test_base_page.py
@@ -2,41 +2,47 @@ from random import randint
 
 import requests
 
-from Tests.Tests_REST_API.BaseClass import BaseParaBank
+from Tests.Tests_REST_API.HomePage import ParaBankHomePage
 
 base_uri = 'https://parabank.parasoft.com/parabank/services/bank'
-account_id = 12345
+customerId = 12656
 amount = 5000
 new_account_id = randint(10000, 99999)
 newAccountType = 0
+username = 'ivan'
+password = 'ivan123'
 
 
-class TestParaBank:
+class TestParaBank(ParaBankHomePage):
 
     def test_get_account_by_id(self):
         headers = {"Accept": "application/json"}
-        response = requests.get(f"{base_uri}/accounts/{account_id}", headers=headers)
+        response = requests.get(f"{base_uri}/accounts/{customerId}", headers=headers)
         assert response.status_code == 200, "           Can't get response 200 for account by id"
 
     def test_get_list_transaction_for_account(self):
         headers = {"Accept": "application/json"}
-        response = requests.get(f"{base_uri}/accounts/{account_id}/transactions", headers=headers)
+        response = requests.get(f"{base_uri}/accounts/{customerId}/transactions", headers=headers)
         assert response.status_code == 200, "           Can't get response 200 for the list transaction for the account"
 
     def test_get_create_transactions_by_amount_for_account(self):
         headers = {"Accept": "application/json"}
-        response = requests.get(f"{base_uri}/accounts/{account_id}/transactions/amount/{amount}", headers=headers)
+        response = requests.get(f"{base_uri}/accounts/{customerId}/transactions/amount/{amount}", headers=headers)
         assert response.status_code == 200, "           Can't get response 200 for create " \
-                                            "transactions by amount for account"
+                                            "transactions with amount for account"
 
     def test_post_new_deposit(self):
         headers = {"Accept": "application/json"}
-        response = requests.post(f"{base_uri}/deposit?accountId={account_id}&amount={amount}", headers=headers)
-        assert response.status_code == 200
+        response = requests.post(f"{base_uri}/deposit?accountId={customerId}&amount={amount}", headers=headers)
+        assert response.status_code == 200, "           Can't get response 200 for new deposit"
 
     def test_post_new_account(self):
         headers = {"Accept": "application/json"}
         response = requests.post(f"{base_uri}/createAccount?customerId={new_account_id}&newAccountType={newAccountType}"
-                                 f"&fromAccountId={account_id}", headers=headers)
-        print(f"{response.text}")
-        # assert response.status_code == 200
+                                 f"&fromAccountId={customerId}", headers=headers)
+        assert response.status_code == 200, "           Can't get response 200 for new account"
+
+    def test_get_login(self):
+        headers = {"Accept": "application/json"}
+        response = requests.get(f"{base_uri}/login/{username}/{password}", headers=headers)
+        assert response.status_code == 200, "           Can't get response 200 for login"
