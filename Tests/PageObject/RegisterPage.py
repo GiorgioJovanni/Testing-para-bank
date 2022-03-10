40fa2d7faaeedde22246e8afcf6bf9b5c1808286
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-03-10 16:49:42 +0300

    Updated tests, added new negative tests

--- Tests/PageObject/RegisterPage.py
+++ Tests/PageObject/RegisterPage.py
@@ -11,38 +11,124 @@ last_name = fake.last_name()
 
 class RegisterPage(BasePage):
     def open_register_page(self):
-        self.should_be_customer_form()
+        self.should_be_panel()
+        self.should_be_form()
 
-    def should_be_customer_form(self):
+    def should_be_panel(self):
         assert self.is_element_present(*RegisterPageLocators.RIGHT_PANEL), "Right panel is not present"
+
+    def should_be_form(self):
         assert self.is_element_present(*RegisterPageLocators.CUSTOMER_FORM), "Customer form is not present"
 
     def register_new_fake_user(self):
+        self.add_text_in_border_first_name()
+        self.add_text_in_border_last_name()
+        self.add_text_in_border_address()
+        self.add_text_in_border_city()
+        self.add_text_in_border_state()
+        self.add_text_in_border_zipcode()
+        self.add_text_in_border_phone()
+        self.add_text_in_border_snn()
+        self.add_text_in_border_username()
+        self.add_text_in_border_password()
+        self.add_text_in_border_confirm()
+        self.press_the_button_register()
+        self.should_not_be_button_register()
+
+    def register_new_fake_user_without_address(self):
+        self.add_text_in_border_first_name()
+        self.add_text_in_border_last_name()
+        self.add_text_in_border_city()
+        self.add_text_in_border_state()
+        self.add_text_in_border_zipcode()
+        self.add_text_in_border_phone()
+        self.add_text_in_border_snn()
+        self.add_text_in_border_username()
+        self.add_text_in_border_password()
+        self.add_text_in_border_confirm()
+        self.press_the_button_register()
+        self.should_be_text_error()
+
+    def register_new_fake_user_without_city(self):
+        self.add_text_in_border_first_name()
+        self.add_text_in_border_last_name()
+        self.add_text_in_border_address()
+        self.add_text_in_border_state()
+        self.add_text_in_border_zipcode()
+        self.add_text_in_border_phone()
+        self.add_text_in_border_snn()
+        self.add_text_in_border_username()
+        self.add_text_in_border_password()
+        self.add_text_in_border_confirm()
+        self.press_the_button_register()
+        self.should_be_text_error()
+
+    def register_new_fake_user_without_state(self):
+        self.add_text_in_border_first_name()
+        self.add_text_in_border_last_name()
+        self.add_text_in_border_address()
+        self.add_text_in_border_city()
+        self.add_text_in_border_zipcode()
+        self.add_text_in_border_phone()
+        self.add_text_in_border_snn()
+        self.add_text_in_border_username()
+        self.add_text_in_border_password()
+        self.add_text_in_border_confirm()
+        self.press_the_button_register()
+        self.should_be_text_error()
+
+    def register_new_fake_user_without_zipcode(self):
+        self.add_text_in_border_first_name()
+        self.add_text_in_border_last_name()
+        self.add_text_in_border_address()
+        self.add_text_in_border_city()
+        self.add_text_in_border_state()
+        self.add_text_in_border_phone()
+        self.add_text_in_border_snn()
+        self.add_text_in_border_username()
+        self.add_text_in_border_password()
+        self.add_text_in_border_confirm()
+        self.press_the_button_register()
+        self.should_be_text_error()
+
+    def add_text_in_border_first_name(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_FIRST_NAME).send_keys(first_name)
+
+    def add_text_in_border_last_name(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_LAST_NAME).send_keys(last_name)
+
+    def add_text_in_border_address(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_ADDRESS).send_keys('New York')
+
+    def add_text_in_border_city(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_CITY).send_keys('New York')
+
+    def add_text_in_border_state(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_STATE).send_keys('New York')
+
+    def add_text_in_border_zipcode(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_ZIPCODE).send_keys('0007')
+
+    def add_text_in_border_phone(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_PHONE).send_keys('+01234241231')
+
+    def add_text_in_border_snn(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_SNN).send_keys('23412341234123')
+
+    def add_text_in_border_username(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_USERNAME).send_keys(f"-1{first_name}1-")
+
+    def add_text_in_border_password(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_PASSWORD).send_keys(password)
+
+    def add_text_in_border_confirm(self):
         self.browser.find_element(*RegisterPageLocators.FIELD_CONFIRM).send_keys(password)
+
+    def press_the_button_register(self):
         self.click_on_button(*RegisterPageLocators.BUTTON_REGISTER)
+
+    def should_not_be_button_register(self):
         assert self.is_not_element_present(*RegisterPageLocators.BUTTON_REGISTER), "Button register is not disappear"
 
-    def negative_register_new_fake_user(self):
-        self.browser.find_element(*RegisterPageLocators.FIELD_FIRST_NAME).send_keys(first_name)
-        self.browser.find_element(*RegisterPageLocators.FIELD_LAST_NAME).send_keys(last_name)
-        # self.browser.find_element(*RegisterPageLocators.FIELD_ADDRESS).send_keys('New York')
-        self.browser.find_element(*RegisterPageLocators.FIELD_CITY).send_keys('New York')
-        self.browser.find_element(*RegisterPageLocators.FIELD_STATE).send_keys('New York')
-        self.browser.find_element(*RegisterPageLocators.FIELD_ZIPCODE).send_keys('0007')
-        self.browser.find_element(*RegisterPageLocators.FIELD_PHONE).send_keys('+01234241231')
-        self.browser.find_element(*RegisterPageLocators.FIELD_SNN).send_keys('23412341234123')
-        self.browser.find_element(*RegisterPageLocators.FIELD_USERNAME).send_keys(f"-1{first_name}1-")
-        self.browser.find_element(*RegisterPageLocators.FIELD_PASSWORD).send_keys(password)
-        self.browser.find_element(*RegisterPageLocators.FIELD_CONFIRM).send_keys(password)
-        self.click_on_button(*RegisterPageLocators.BUTTON_REGISTER)
+    def should_be_text_error(self):
         assert self.is_element_present(*RegisterPageLocators.TEXT_ERROR), "Text error is not present"
--- Tests/Tests UI/test_register_page.py
+++ Tests/Tests UI/test_register_page.py
@@ -12,14 +12,6 @@ def test_guest_can_register(browser):
     page.register_new_fake_user()
 
 
-def test_negative_guest_can_register(browser):
-    page = RegisterPage(browser, base_link)
-    page.open()
-    page.click_on_button(*BasePageLocators.BUTTON_REGISTER)
-    page.open_register_page()
-    page.negative_register_new_fake_user()
-
-
 def test_guest_use_buttons_gohome_contact_aboutus_from_register_page(browser):
     page = RegisterPage(browser, base_link)
     page.open()
@@ -32,3 +24,33 @@ def test_guest_use_buttons_gohome_contact_aboutus_from_register_page(browser):
     page.open()
     page.click_on_button(*BasePageLocators.BUTTON_REGISTER)
     page.button_go_aboutus()
+
+
+class TestNegative:
+    def test_negative_guest_can_register_address(self, browser):
+        page = RegisterPage(browser, base_link)
+        page.open()
+        page.click_on_button(*BasePageLocators.BUTTON_REGISTER)
+        page.open_register_page()
+        page.register_new_fake_user_without_address()
+
+    def test_negative_guest_can_register_city(self, browser):
+        page = RegisterPage(browser, base_link)
+        page.open()
+        page.click_on_button(*BasePageLocators.BUTTON_REGISTER)
+        page.open_register_page()
+        page.register_new_fake_user_without_city()
+
+    def test_negative_guest_can_register_zipcode(self, browser):
+        page = RegisterPage(browser, base_link)
+        page.open()
+        page.click_on_button(*BasePageLocators.BUTTON_REGISTER)
+        page.open_register_page()
+        page.register_new_fake_user_without_zipcode()
+
+    def test_negative_guest_can_register_state(self, browser):
+        page = RegisterPage(browser, base_link)
+        page.open()
+        page.click_on_button(*BasePageLocators.BUTTON_REGISTER)
+        page.open_register_page()
+        page.register_new_fake_user_without_state()
