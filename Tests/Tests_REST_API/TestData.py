a32ab21d06f806daf342ac71d545d6dd3fd8437d
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-03-15 17:11:19 +0300

    Updated tests

--- Tests/Tests_REST_API/TestData.py
+++ Tests/Tests_REST_API/TestData.py
@@ -2,20 +2,24 @@ from random import randint
 
 
 class DataTest:
-    customerId = 12656
-    amount = 5000
-    new_account_id = randint(10000, 99999)
-    newAccountType = 0
+    customerId = '12656'
+    amount = '5000'
+    new_account_id = str(randint(10000, 99999))
+    newAccountType = '0'
     username = 'ivan'
     password = 'ivan123'
 
 
 class DataEndPoints:
     base_uri = 'https://parabank.parasoft.com/parabank/services/bank'
-    getAccountId = f"/accounts/{DataTest.customerId}"
-    getListTransactionForAccount = f"/accounts/{DataTest.customerId}/transactions"
-    createTransactionsByAmountForAccount = f"/accounts/{DataTest.customerId}/transactions/amount/{DataTest.amount}"
-    newDeposit = f"/deposit?accountId={DataTest.customerId}&amount={DataTest.amount}"
-    newAccount = f"/createAccount?customerId={DataTest.new_account_id}&newAccountType={DataTest.newAccountType}" \
-                 f"&fromAccountId={DataTest.customerId}"
-    login = f"/login/{DataTest.username}/{DataTest.password}"
+    getAccountId = f"/accounts/"
+    getListTransactionForAccount1 = f"/accounts/"
+    getListTransactionForAccount2 = "/transactions"
+    createTransactionsByAmountForAccount1 = f"/accounts/"
+    createTransactionsByAmountForAccount2 = "/transactions/amount/"
+    newDeposit1 = f"/deposit?accountId="
+    newDeposit2 = "&amount="
+    newAccount1 = f"/createAccount?customerId="
+    newAccount2 = f"&newAccountType="
+    newAccount3 = f"&fromAccountId="
+    login = f"/login/"
--- Tests/Tests_REST_API/test_base_page.py
+++ Tests/Tests_REST_API/test_base_page.py
@@ -5,28 +5,34 @@ from Tests.Tests_REST_API.TestData import DataEndPoints, DataTest
 class TestParaBank(ParaBankHomePage):
 
     def test_get_account_by_id(self):
-        response = self.get(f"{DataEndPoints.base_uri}{DataEndPoints.getAccountId}")
+        response = self.get(f"{DataEndPoints.base_uri + DataEndPoints.getAccountId + DataTest.customerId}")
         assert response.status_code == 200, "           Can't get response 200 for account by id"
 
     def test_get_list_transaction_for_account(self):
-        response = self.get(f"{DataEndPoints.base_uri}{DataEndPoints.getListTransactionForAccount}")
+        response = self.get(f"{DataEndPoints.base_uri + DataEndPoints.getListTransactionForAccount1}"
+                            f"{DataTest.customerId + DataEndPoints.getListTransactionForAccount2}")
         assert response.status_code == 200, "           Can't get response 200 for the list transaction for the account"
 
     def test_get_create_transactions_by_amount_for_account(self):
-        response = self.get(f"{DataEndPoints.base_uri}{DataEndPoints.createTransactionsByAmountForAccount}")
+        response = self.get(f"{DataEndPoints.base_uri + DataEndPoints.createTransactionsByAmountForAccount1}"
+                            f"{DataTest.customerId + DataEndPoints.createTransactionsByAmountForAccount2}"
+                            f"{DataTest.amount}")
         assert response.status_code == 200, "           Can't get response 200 for create " \
                                             "transactions with amount for account"
 
     def test_post_new_deposit(self):
-        response = self.post(f"{DataEndPoints.base_uri}{DataEndPoints.newDeposit}")
+        response = self.post(f"{DataEndPoints.base_uri + DataEndPoints.newDeposit1 + DataTest.customerId}"
+                             f"{DataEndPoints.newDeposit2 + DataTest.amount}")
         assert response.status_code == 200, "           Can't get response 200 for new deposit"
 
     def test_post_new_account(self):
-        response = self.post(f"{DataEndPoints.base_uri}{DataEndPoints.newAccount}")
+        response = self.post(f"{DataEndPoints.base_uri + DataEndPoints.newAccount1 + DataTest.new_account_id}"
+                             f"{DataEndPoints.newAccount2 + DataTest.newAccountType}"
+                             f"{DataEndPoints.newAccount3 + DataTest.customerId}")
         assert response.text == f'Could not create new account for customer {DataTest.new_account_id}' \
                                 f' from account 12656'
         assert response.status_code == 200, "           Can't get response 200 for new account"
 
     def test_get_login(self):
-        response = self.get(f"{DataEndPoints.base_uri}{DataEndPoints.login}")
+        response = self.get(f"{DataEndPoints.base_uri + DataEndPoints.login + DataTest.username}/{DataTest.password}")
         assert response.status_code == 200, "           Can't get response 200 for login"
